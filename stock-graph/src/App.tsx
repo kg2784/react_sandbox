22ada18ee9fe7ad3ba0cfe6b82240d11ca89efd0
Taka <sedtaka@gmail.com>
2021-06-23 12:43:20 +0900

    add graphs

--- stock-graph/src/App.tsx
+++ stock-graph/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import './StockGraph.css';
+
+const StockGraph = ({color}: {color: "green"|"red"}) => (
+    <div className="stock-graph-component">
+        <div className={"stock-logo"}><img src={"mastercard.png"}/></div>
+        <div className={"stock-name"}>Company</div>
+        <div className={"stock-graph"}><img src={`sample-graph-${color}.png`}/></div>
+        <img src={`price-diff-${color}.png`}/>
+        <div className={"stock-price"} style={{color}}>¥888,888</div>
+        <div className={"stock-price-diff"}>¥88.88</div>
+    </div>
+);
 
 function App() {
   return (
-    <div className="App">Hello</div>
-  );
+      <div>
+          <StockGraph color={"green"}/>
+          <StockGraph color={"red"}/>
+      </div>
+    )
 }
 
 export default App;
